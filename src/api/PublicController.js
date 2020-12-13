7d6f659f5e057107fadd382fb650dab7fed2fe19
yh <yuehong_web@163.com>
2020-12-13 20:29:50 +0800

    first commit

---
+++ src/api/PublicController.js
@@ -0,0 +1,20 @@
+const svgCaptcha = require('svg-captcha')
+
+class PublicController {
+  constructor() {}
+  async getCaptcha(ctx) {
+    const captcha = svgCaptcha.create({
+      size:4,
+      ignoreChars:'0o1i',
+      noise:3,
+      color:true ,
+      background:'#f2f2f2'
+    });
+    ctx.body = {
+      code:200,
+      data: captcha.data
+    }
+  }
+}
+
+export default new PublicController()
